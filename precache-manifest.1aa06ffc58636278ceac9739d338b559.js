33e46b9cdf57ce24a3816e433906bc155580e5f5
caiohman <4341494f@usp.br>
2020-07-28 08:36:06 -0300

    Updates

---
+++ precache-manifest.1aa06ffc58636278ceac9739d338b559.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c4719f045b8f685950f43e7fd4eea4",
+    "url": "/caio-perfil/index.html"
+  },
+  {
+    "revision": "27d2be7f26b0b7060a9d",
+    "url": "/caio-perfil/static/css/main.fd8dbe28.chunk.css"
+  },
+  {
+    "revision": "218fd80f2f0576f215df",
+    "url": "/caio-perfil/static/js/2.818a9d45.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/caio-perfil/static/js/2.818a9d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d2be7f26b0b7060a9d",
+    "url": "/caio-perfil/static/js/main.1fd8a429.chunk.js"
+  },
+  {
+    "revision": "40a2a6ccb1fc57b797d0",
+    "url": "/caio-perfil/static/js/runtime-main.54dc4838.js"
+  },
+  {
+    "revision": "b6f4362ff06d19a04c1e5eb43202ea1d",
+    "url": "/caio-perfil/static/media/picture-profile.b6f4362f.jpg"
+  }
+]);
